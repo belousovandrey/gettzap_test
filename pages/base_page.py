77b0168fa115fc98a94244dde17c336d90d97bf4
andrey <58342362+belousovandrey@users.noreply.github.com>
2023-04-01 17:30:14 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -57,3 +57,20 @@ class BasePage:
         action = ActionChains(self.driver)
         action.move_to_element(element)
         action.perform()
+        
+        def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
