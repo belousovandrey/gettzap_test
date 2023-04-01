b198856a0239d02b6f2e4a6463f68a3d1fa9d7f8
APC\Andrey <belousovandrey3105@gmail.com>
2023-04-01 12:32:25 +0300

    error login_button

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -4,9 +4,18 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     REGIONS_BUTTON = (By.CSS_SELECTOR, '[class="MuiBackdrop-root geo-tooltip__backdrop MuiBackdrop-invisible"]')
     COOKIE_BUTTON = (By.CSS_SELECTOR, '[class="MuiButtonBase-root MuiIconButton-root icon-button__root icon-button__root_size_default cookies-notification__button"]')
+    # login_form
     MAIN_LOGIN_BUTTON = (By.CSS_SELECTOR, '[class="MuiButtonBase-root MuiButton-root MuiButton-outlined customer-header-login__button-initial"]')
     LOGIN = (By.CSS_SELECTOR, '[class="MuiInputBase-input MuiInput-input text-field__input"]')
     PASSWORD = (By.CSS_SELECTOR, '[class="MuiInputBase-input MuiInput-input text-field__input password-input__input MuiInputBase-inputAdornedEnd"]')
-    LOGIN_BUTTON = (By.CSS_SELECTOR, '[class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedSecondary"]')
+    LOGIN_BUTTON = (By.XPATH, '/html/body/div[3]/div[3]/div/div[2]/form/div[2]')
     LOGIN_ERROR = (By.CSS_SELECTOR, '[class="MuiFormHelperText-root Mui-error MuiFormHelperText-filled"]')
-    LOGIN_BUTTON = (By.CSS_SELECTOR, '[class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedSecondary"]')
+    # registratin_form
+    REG_USER_BUTTON = (By.CSS_SELECTOR, 'body > div.MuiDialog-root > div.MuiDialog-container.auth-dialog__dialog-container.MuiDialog-scrollPaper > div > div.dialog__content.auth-dialog__dialog-content > div.MuiFormControl-root.MuiFormControl-marginNormal.MuiFormControl-fullWidth > p:nth-child(2) > button')
+    REG_COMPANY_BUTTON = (By.CSS_SELECTOR, 'body > div.MuiDialog-root > div.MuiDialog-container.auth-dialog__dialog-container.MuiDialog-scrollPaper > div > div.dialog__content.auth-dialog__dialog-content > div.MuiFormControl-root.MuiFormControl-marginNormal.MuiFormControl-fullWidth > p:nth-child(3) > button')
+    FIO_USER = (By.CSS_SELECTOR, 'body > div.MuiDialog-root > div.MuiDialog-container.auth-dialog__dialog-container.MuiDialog-scrollPaper > div > div.dialog__content.auth-dialog__dialog-content > form > div.signup__inputs-slot > div:nth-child(1) > div > input')
+    PHONE_USER = (By.CSS_SELECTOR, 'body > div.MuiDialog-root > div.MuiDialog-container.auth-dialog__dialog-container.MuiDialog-scrollPaper > div > div.dialog__content.auth-dialog__dialog-content > form > div.signup__inputs-slot > div:nth-child(2) > div > input')
+    EMAIL_USER = (By.CSS_SELECTOR, 'body > div.MuiDialog-root > div.MuiDialog-container.auth-dialog__dialog-container.MuiDialog-scrollPaper > div > div.dialog__content.auth-dialog__dialog-content > form > div.signup__inputs-slot > div:nth-child(3) > div > input')
+    AGREEMENT_BOX = (By.CSS_SELECTOR, 'body > div.MuiDialog-root > div.MuiDialog-container.auth-dialog__dialog-container.MuiDialog-scrollPaper > div > div.dialog__content.auth-dialog__dialog-content > label > span.MuiTypography-root.MuiFormControlLabel-label.MuiTypography-body1 > p')
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, '#registration-btn')
+    LOGIN_BUTTON = (By.CSS_SELECTOR, '[class="jss458"]')
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,13 +1,20 @@
 import time
 
-from pages.base_page import BasePage
 from pages.main_page import MainPage
 
 
-def test_login_page(driver):
-    login_page = MainPage(driver, 'https://gettzap.ru/')
-    login_page.open()
-    login_page.check_regions_cookies()
-    result = login_page.login_page_account()
-    assert "Запрашиваемый ресурс не был найден Неверный логин или пароль" != result.text ,"Запрашиваемый ресурс не был найден Неверный логин или пароль"
+class TestLoginPage:
 
+    def test_login_page(self, driver):
+        login_page = MainPage(driver, 'https://gettzap.ru/')
+        login_page.open()
+        login_page.check_regions_cookies()
+        result = login_page.login_page_account()
+        assert "Запрашиваемый ресурс не был найден Неверный логин или пароль" != result.text, "Запрашиваемый ресурс не был найден Неверный логин или пароль"
+
+    def test_registration_page(self,driver):
+        registration_page = MainPage(driver, 'https://gettzap.ru/')
+        registration_page.open()
+        registration_page.check_regions_cookies()
+        registration_page.registration_account()
+        time.sleep(3)
