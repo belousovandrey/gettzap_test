727cc9a7335a2ec66e1d7e04ff56f3cd5bb8cdd4
DESKTOP-4R0N8H5\Andrey <belousovandrey3105@gmail.com>
2023-04-01 19:33:16 +0300

    fixed error add test_link

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 import time
 
+import requests
 from selenium.webdriver import Keys
 
 from locators.main_page_locators import MainPageLocators
@@ -18,7 +19,8 @@ class MainPage(BasePage):
         self.element_is_visible(self.locators.LOGIN).send_keys('bel3105@rambler.ru')
         self.element_is_visible(self.locators.PASSWORD).send_keys('FYlAEGoL')
         self.element_is_visible(self.locators.LOGIN_BUTTON).click()
-        result = self.element_is_present(self.locators.LOGIN_ERROR)
+        time.sleep(0.25)
+        result = self.element_is_not_present(self.locators.LOGIN_ERROR)
         return result
 
     def registration_account(self):
@@ -29,3 +31,15 @@ class MainPage(BasePage):
         self.element_is_visible(self.locators.EMAIL_USER).send_keys('bel3105@rambler.ru')
         self.element_is_visible(self.locators.AGREEMENT_BOX).click()
         self.element_is_visible(self.locators.REGISTRATION_BUTTON).click()
+
+    def check_new_tab_simple_link(self):
+        simple_link = self.element_is_visible(self.locators.ORDER_LINK)
+        link_href = simple_link.get_attribute('href')
+        request = requests.get(link_href)
+        if request.status_code == 200:
+            self.element_is_visible(self.locators.ORDER_LINK_BUTTON).click()
+            self.driver.switch_to.window(self.driver.window_handles[-1])
+            url = self.driver.current_url
+            return link_href, url
+        else:
+            return link_href, request.status_code
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -10,11 +10,20 @@ class TestLoginPage:
         login_page.open()
         login_page.check_regions_cookies()
         result = login_page.login_page_account()
-        assert "Запрашиваемый ресурс не был найден Неверный логин или пароль" != result.text, "Запрашиваемый ресурс не был найден Неверный логин или пароль"
+        assert result != True, "Запрашиваемый ресурс не был найден Неверный логин или пароль"
 
-    def test_registration_page(self,driver):
+    def test_registration_page(self, driver):
         registration_page = MainPage(driver, 'https://gettzap.ru/')
         registration_page.open()
         registration_page.check_regions_cookies()
         registration_page.registration_account()
         time.sleep(3)
+
+    def test_check_link(self, driver):
+        links_page = MainPage(driver, 'https://gettzap.ru/')
+        links_page.open()
+        links_page.check_regions_cookies()
+        links_page.login_page_account()
+        time.sleep(3)
+        href_link, current_url = links_page.check_new_tab_simple_link()
+        assert href_link == current_url, "the link is broken or url is incorrect"
