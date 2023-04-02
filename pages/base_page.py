f1c09d086ce26a29e92c905a7ecf35cce2529506
DESKTOP-4R0N8H5\Andrey <belousovandrey3105@gmail.com>
2023-04-02 11:41:17 +0300

    add allure update requirements.txt

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.common import TimeoutException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,56 +10,70 @@ class BasePage:
         self.driver = driver
         self.url = url
 
+    @allure.step('Open a browser')
     def open(self):
         self.driver.get(self.url)
 
+    @allure.step('element_is_visible')
     def element_is_visible(self, locator, timeout=5):
         self.go_to_element(self.element_is_present(locator))
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
+    @allure.step('elements_are_visible')
     def elements_are_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
+    @allure.step('element_is_present')
     def element_is_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
+    @allure.step('elements_are_present')
     def elements_are_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
 
+    @allure.step('element_is_not_visible')
     def element_is_not_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
 
+    @allure.step('element_is_clickable')
     def element_is_clickable(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
+    @allure.step('go_to_element')
     def go_to_element(self, element):
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
+    @allure.step('action_double_click')
     def action_double_click(self, element):
         action = ActionChains(self.driver)
         action.double_click(element)
         action.perform()
 
+    @allure.step('action_right_click')
     def action_right_click(self, element):
         action = ActionChains(self.driver)
         action.context_click(element)
         action.perform()
 
+    @allure.step('action_drag_and_drop_by_offset')
     def action_drag_and_drop_by_offset(self, element, x_coords, y_coords):
         action = ActionChains(self.driver)
         action.drag_and_drop_by_offset(element, x_coords, y_coords)
         action.perform()
 
+    @allure.step('action_drag_and_drop_to_element')
     def action_drag_and_drop_to_element(self, what, where):
         action = ActionChains(self.driver)
         action.drag_and_drop(what, where)
         action.perform()
 
+    @allure.step('action_move_to_element')
     def action_move_to_element(self, element):
         action = ActionChains(self.driver)
         action.move_to_element(element)
         action.perform()
 
+    @allure.step('element_is_not_present')
     def element_is_not_present(self, locator, timeout=4):
         try:
             wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 import time
 
+import allure
 import requests
 from selenium.webdriver import Keys
 
@@ -10,10 +11,12 @@ from pages.base_page import BasePage
 class MainPage(BasePage):
     locators = MainPageLocators()
 
+    @allure.step('regions_cookies')
     def check_regions_cookies(self):
         self.element_is_visible(self.locators.REGIONS_BUTTON).click()
         self.element_is_visible(self.locators.COOKIE_BUTTON).click()
 
+    @allure.step('login_page_account')
     def login_page_account(self):
         self.element_is_visible(self.locators.MAIN_LOGIN_BUTTON).click()
         self.element_is_visible(self.locators.LOGIN).send_keys('bel3105@rambler.ru')
@@ -23,6 +26,7 @@ class MainPage(BasePage):
         result = self.element_is_not_present(self.locators.LOGIN_ERROR)
         return result
 
+    @allure.step('registration_account')
     def registration_account(self):
         self.element_is_visible(self.locators.MAIN_LOGIN_BUTTON).click()
         self.element_is_visible(self.locators.REG_USER_BUTTON).click()
@@ -32,12 +36,27 @@ class MainPage(BasePage):
         self.element_is_visible(self.locators.AGREEMENT_BOX).click()
         self.element_is_visible(self.locators.REGISTRATION_BUTTON).click()
 
-    def check_new_tab_simple_link(self):
+    @allure.step('order_link')
+    def order_link(self):
         simple_link = self.element_is_visible(self.locators.ORDER_LINK)
-        link_href = simple_link.get_attribute('href')
+        with allure.step('get_attribute("href")'):
+            link_href = simple_link.get_attribute('href')
         request = requests.get(link_href)
         if request.status_code == 200:
             self.element_is_visible(self.locators.ORDER_LINK_BUTTON).click()
+            self.driver.switch_to.window(self.driver.window_handles[-1])
+            url = self.driver.current_url
+            return link_href, url
+        else:
+            return link_href, request.status_code
+
+    @allure.step('favorites_link')
+    def favorites_link(self):
+        simple_link = self.element_is_visible(self.locators.FAVORITES_LINK)
+        link_href = simple_link.get_attribute('href')
+        request = requests.get(link_href)
+        if request.status_code == 200:
+            self.element_is_visible(self.locators.FAVORITES_LINK_BUTTON).click()
             self.driver.switch_to.window(self.driver.window_handles[-1])
             url = self.driver.current_url
             return link_href, url
